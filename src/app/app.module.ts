c1fc3a916c720aa6cc206997067089db3afb846c
Sascha Lehmann <sascha.lehmann92@gmail.com>
2022-07-28 11:30:56 +0200

    feat(app): added lazy container component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LazyContainerComponent } from './lazy-container/lazy-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LazyContainerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/lazy-container/lazy-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lazy-container',
+  templateUrl: './lazy-container.component.html',
+  styleUrls: ['./lazy-container.component.scss']
+})
+export class LazyContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
