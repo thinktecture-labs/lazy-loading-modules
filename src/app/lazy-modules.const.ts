7863ecdaef6bb1628839a2324d0c86266757b329
Sascha Lehmann <sascha.lehmann92@gmail.com>
2022-07-28 15:22:40 +0200

    fix(app): hide import in function to prevent all modules from being fetched at once

--- src/app/lazy-modules.const.ts
+++ src/app/lazy-modules.const.ts
@@ -1,6 +1,6 @@
-export const LAZY_MODULES: Map<string, Promise<unknown>> = new Map([
-  ['A', import('@lazy/lazy-a').then(m => m.LazyAModule)],
-  ['B', import('@lazy/lazy-b').then(m => m.LazyBModule)],
-  ['C', import('@lazy/lazy-c').then(m => m.LazyCModule)],
-  ['D', import('@lazy/lazy-d').then(m => m.LazyDModule)]
+export const LAZY_MODULE_IMPORT_FUNCTIONS: Map<string, () => Promise<unknown>> = new Map([
+  ['A', async () => await import('@lazy/lazy-a').then(m => m.LazyAModule)],
+  ['B', async () => await import('@lazy/lazy-b').then(m => m.LazyBModule)],
+  ['C', async () => await import('@lazy/lazy-c').then(m => m.LazyCModule)],
+  ['D', async () => await import('@lazy/lazy-d').then(m => m.LazyDModule)]
 ])
--- src/app/module-loader.service.ts
+++ src/app/module-loader.service.ts
@@ -1,5 +1,5 @@
 import {Compiler, Injectable, Injector, NgModuleFactory} from '@angular/core';
-import {LAZY_MODULES} from "./lazy-modules.const";
+import {LAZY_MODULE_IMPORT_FUNCTIONS} from "./lazy-modules.const";
 import {SectionService} from "./section.service";
 
 @Injectable({
@@ -11,12 +11,16 @@ export class ModuleLoaderService {
   }
 
   async loadModule(key: string): Promise<void> {
-    const lazyModule = await LAZY_MODULES.get(key);
-    if (key === "D") {
-      await this.delay(3000);
+    const loaderFunc = await LAZY_MODULE_IMPORT_FUNCTIONS.get(key);
+    if (loaderFunc) {
+      const lazyModule = await loaderFunc();
+      if (key === "D") {
+        await this.delay(3000);
+      }
+      const moduleFacotry = await this.loadModuleFactory(lazyModule);
+      const module = moduleFacotry.create(this.injector);
     }
-    const moduleFacotry = await this.loadModuleFactory(lazyModule);
-    const module = moduleFacotry.create(this.injector);
+
   }
 
   private async loadModuleFactory(t: any): Promise<NgModuleFactory<unknown>> {
